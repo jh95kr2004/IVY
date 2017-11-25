554ff65c818e030849cdac3b6f44004d4d54c0af
Junghoon Jang <junghoonjang@Jungui-iMac.local>
2017-11-26 01:57:53 +0900

    Implement student addition function

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -1,4 +1,32 @@
 $(function() {
+  $("#highSchoolSearch").on("input", function() {
+    $("#saveButton").attr("disabled", "true");
+    if($(this).val() == "") {
+      $("#highSchoolDiv table tbody").html("");
+      return;
+    }
+    $.get("/consult/highschool", { keyword: $(this).val() })
+    .done(function(data) {
+      var tbody = $("#highSchoolDiv table tbody");
+      $(tbody).html("");
+      for(var i = 0; i < data.length; i++) {
+        var tr = $("<tr></tr>");
+        $(tr).html("<td>" + data[i]["name"] + "</td><td>" + data[i]["location"] + "</td><td class='code'>" + data[i]["_id"] + "</td>")
+        $(tr).click(function() {
+          if($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
+            $("#saveButton").attr("disabled", "true");
+          } else {
+            $(this).siblings().removeClass("selected");
+            $(this).addClass("selected");
+            $("#saveButton").removeAttr("disabled");
+          }
+        });
+        $(tr).appendTo(tbody);
+      }
+    });
+  });
+
   $(".form-control").keyup(function() {
     if(!($("#fullname").is(":valid")) || !($("#grade").is(":valid"))) {
       $("#nextButton").attr("disabled", "true");
@@ -17,11 +45,6 @@ $(function() {
     $("#newTitle").text("New Student - High School");
     $("#newDescription").text("Select the student's current high school.");
   });
-
-  $(".resultContainer tr").click(function() {
-    if($(this).hasClass("selected")) $("#saveButton").removeAttr("disabled");
-    else $("#saveButton").attr("disabled", "true");
-  })
 
   $("#backButton").click(function() {
     $("#highSchoolDiv").addClass("d-none");
--- routes/consult.js
+++ routes/consult.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 var url = "mongodb://localhost:27017/IVY";
 
+var authenticate = function(req, res, next) {
+  if(req.session.user) {
+    if(req.session.user.permission != 'consultant')
+      res.layout('layout', {title:"IVY: No permission", head:""}, {body:{block:"caution", data:{title: "No Permission", description:"You don't have a permission to access consulting service."}}});
+    else
+      next();
+  }
+  else
+    res.redirect('/login');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  checkLoginConsult(req, res);
+router.get('/', authenticate, function(req, res, next) {
   var studentListHtml = "";
   MongoClient.connect(url, function(err, db) {
     if(err) throw err;
@@ -16,16 +27,16 @@ router.get('/', function(req, res, next) {
   res.layout('layout', {title:"IVY: Consult", head:""}, {body:{block:"consult"}});
 });
 
-router.post('/new', function(req, res, next) {
-  checkLoginConsult(req, res);
+router.post('/new', authenticate, function(req, res, next) {
   if(req.body.name.length == 0 || req.body.grade < 10 || req.body.grade > 12) res.send("0");
   else {
-    var student = { name: req.body.name, grade: req.body.grade, highschool: req.body.highschool }
+    var student = { name: req.body.name, grade: req.body.grade, highschool: new ObjectID(req.body.highschool) }
     MongoClient.connect(url, function(err, db) {
       if(err) throw err;
       db.collection("students").insertOne(student, function(err, res) {
         if(err) throw err;
-        db.collection("accounts").updateOne({ email: req.session.user.email }, {}, function(err, res) {
+        db.collection("accounts").updateOne({ email: req.session.user.email }, { $push: { students: new ObjectID(res.ops._id) } }, function(err, res) {
+          db.close();
         });
         db.close();
       })
@@ -34,22 +45,21 @@ router.post('/new', function(req, res, next) {
   }
 })
 
-router.get('/new', function(req, res, next) {
-  checkLoginConsult(req, res);
+router.get('/new', authenticate, function(req, res, next) {
   res.layout('layout', {title:"IVY: New Student", head:'<script src="/javascripts/new.js"></script>'}, {body:{block:"new"}});
 });
 
-router.get('/edit/:id', function(req, res, next) {
-  checkLoginConsult(req, res);
+router.get('/edit/:id', authenticate, function(req, res, next) {
 });
 
-function checkLoginConsult(req, res) {
-  if(req.session.user) {
-    if(req.session.user.permission != 'consultant')
-      res.layout('layout', {title:"IVY: No permission", head:""}, {body:{block:"caution", data:{title: "No Permission", description:"You don't have a permission to access consulting service."}}});
-  }
-  else
-    res.redirect('../login');
-}
+router.get('/highschool', authenticate, function(req, res, next) {
+  var keyword = req.query.keyword;
+  MongoClient.connect(url, function(err, db) {
+    if(err) throw err;
+    db.collection("highschools").find({$or: [{name: { "$regex": keyword, "$options": "i"}}, {location: { "$regex": keyword, "$options": "i"}}]}).toArray(function(err, result) {
+      res.send(result);
+    });
+  });
+});
 
 module.exports = router;
